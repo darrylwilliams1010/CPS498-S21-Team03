dabfc8044e6ce724cf2b01261134c23c2690fcb4
Darryl Williams <45323165+darrylwilliams1010@users.noreply.github.com>
2021-04-21 22:51:45 -0400

    Add files via upload
    first module. let me know if you need the other files

---
+++ CameraScript.cs
@@ -0,0 +1,36 @@
+﻿/* Author: Darryl Williams 
+ * About: moving script for the main camera
+ * This allows user to rotate the camera 
+ * with WASD keys
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float speedH = 2.0f; // speed for the horizontal rotation
+    public float speedV = 2.0f; // speed for vertical rotation
+    private float yaw = 0.0f; // place holder for horizontal input 
+    private float pitch = 0.0f; // place holder for vertical input
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        yaw += speedH * Input.GetAxis("Horizontal"); // gets input of horizontal keys 
+        pitch -= speedV * Input.GetAxis("Vertical"); // gets input of vertical keys
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f); // set the inputs of both yaw and pitch
+    }
+}
+
---
+++ zoom.cs
@@ -0,0 +1,51 @@
+﻿/*Author: Darryl Williams
+ * About: Zoom in script for the main camera
+ * allows user to change the field of view with key inputs
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoom : MonoBehaviour
+{
+    private Camera cam;    
+    private float startingFOV;
+
+    public float minFOV;  // limits how far user can zoom out
+    public float maxFOV;   // limits how far the user can zoom in 
+    public float zoomRate; // sets how fast the camera zooms in or out 
+
+    private float currentFOV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>(); // access the main camera 
+        startingFOV = cam.fieldOfView; // sets the starting view of the camera to cam view
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentFOV = cam.fieldOfView;
+
+        // reset the view to the starting view 
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            currentFOV = startingFOV; 
+        }
+        // zooms out with key O
+        if (Input.GetKey(KeyCode.O))
+        {
+            currentFOV += zoomRate;
+        }
+        // zooms in with key I
+        if (Input.GetKey(KeyCode.I))
+        {
+            currentFOV -= zoomRate;
+        }
+        // set the current field
+        currentFOV = Mathf.Clamp(currentFOV, minFOV, maxFOV);
+        cam.fieldOfView = currentFOV;
+    }
+}
